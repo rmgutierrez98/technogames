cf6477c8cbe630cfc1cdc2f2094044a500b53ee7
rmgutierrez98 <rmgutierrez0@misena.edu.co>
2017-09-20 18:01:57 -0500

    base

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+ 
+gulp.task('games', function() {
+  gulp.src('./')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      port: 8500
+    }));
+});
